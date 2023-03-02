dd76b4439578cc210859aff4a4397bfb93b834cc
aryansingh920 <aryansingh920@outlook.com>
2023-03-02 12:17:59 +0530

    Initial Commit

---
+++ Treko-New/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Treko-New
+//
+//  Created by Aryan Singh on 02/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
