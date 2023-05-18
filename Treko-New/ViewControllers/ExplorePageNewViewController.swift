afe021d2c9ba52728640b8622c5976e57af50e6d
aryansingh920 <aryansingh920@outlook.com>
2023-05-18 20:37:32 +0530

    new

---
+++ Treko-New/ViewControllers/ExplorePageNewViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ExplorePageNewViewController.swift
+//  Treko-New
+//
+//  Created by Aryan Singh on 18/05/23.
+//
+
+import UIKit
+
+class ExplorePageNewViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
+
+    private let cellIdentifier = "CellIdentifier"
+    private var collectionView: UICollectionView!
+    private var searchBar: UISearchBar!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Create the collection view layout
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 2
+        layout.minimumLineSpacing = 2
+
+        // Create the collection view
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        view.addSubview(collectionView)
+
+        // Create the search bar
+        searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Search"
+        searchBar.delegate = self
+        view.addSubview(searchBar)
+
+        // Set up constraints
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    // MARK: - Collection View Data Source
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9 // Change this value to the desired number of items
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        cell.backgroundColor = UIColor.lightGray
+
+        // Configure the cell with an image
+        let imageView = UIImageView(image: UIImage(named: "your_image_name"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.frame = cell.contentView.bounds
+        cell.contentView.addSubview(imageView)
+
+        return cell
+    }
+
+    // MARK: - Collection View Delegate Flow Layout
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width - 6) / 3 // Adjust the spacing as per your requirement
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+
+    // MARK: - Search Bar Delegate
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Perform search or filter operation based on the search text
+    }
+}
+
