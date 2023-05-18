351679746d5e598f40f5eb18c5be35182dd1b77a
aryansingh920 <aryansingh920@outlook.com>
2023-05-18 21:32:27 +0530

    latest

--- Treko-New/ViewControllers/AddPostViewController.swift
+++ Treko-New/ViewControllers/AddPostViewController.swift
@@ -1,15 +1,8 @@
-//
-//  AddPostViewController.swift
-//  Treko-New
-//
-//  Created by Aryan Singh on 06/04/23.
-//
-
 import UIKit
 
-class AddPostViewController: UIViewController {
-
-    @IBOutlet var imageLink: UITextField!
+class AddPostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
     @IBOutlet var postDescription: UITextField!
     @IBOutlet var location: UITextField!
     
@@ -18,44 +11,83 @@ class AddPostViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+    }
+    
+    @IBAction func selectImageTapped(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alertController = UIAlertController(title: "Add Image", message: nil, preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Take Photo", style: .default) { (_) in
+            imagePicker.sourceType = .camera
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+        let photoLibraryAction = UIAlertAction(title: "Choose from Library", style: .default) { (_) in
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func addPostClicked(_ sender: Any) {
-        if(imageLink.text != "" && postDescription.text != "" && location.text != ""){
+        if let image = imageView.image,
+           let description = postDescription.text,
+           let location = location.text,
+           !description.isEmpty,
+           !location.isEmpty {
             let routeLink = "/addPost/\(defaults.string(forKey: "user")!)"
             let parameters = [
-                "images":imageLink.text!,
-                "description":postDescription.text!,
-                "location":location.text!
-                ]
+                "description": description,
+                "location": location
+            ]
             apiCall.POST(parameters: parameters, route: routeLink) { result in
                 switch result {
                 case .success(let responseData):
-                    if(responseData["msg"]! == "Post added"){
+                    if responseData["msg"] == "Post added" {
                         self.handleError(msg: "Post Added")
-                    }else{
-                        self.handleError(msg: responseData["msg"]!)
+                    } else {
+                        self.handleError(msg: responseData["msg"] ?? "Unknown error")
                     }
                 case .failure(let error):
-                    print("Err",error)
+                    print("Error:", error)
                     self.handleError(msg: "Error in server")
                 }
             }
-        }else{
-            print("Empty field")
-            handleError(msg: "Error empty Field")
-        }
-    }
-    func handleError(msg:String){
-        DispatchQueue.main.async {
-            let alert = UIAlertController(title: "Msg", message: msg, preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel,handler: {action in print("Tapped dismiss")}))
-            self.present(alert,animated: true)
+        } else {
+            handleError(msg: "Error: Empty field")
         }
     }
     
-
+    func handleError(msg: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Message", message: msg, preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel) { _ in
+                print("Tapped dismiss")
+            })
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let pickedImage = info[.originalImage] as? UIImage {
+            imageView.image = pickedImage
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
